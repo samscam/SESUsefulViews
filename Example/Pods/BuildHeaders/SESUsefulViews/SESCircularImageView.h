5739d24cc8c59fa3932cc55c07699d57a38b6416
Sam Easterby-Smith <Sam.Easterby-Smith01.ext@bbc.co.uk>
2015-01-06 16:11:50 +0000

    A load of expecta related stuff got deleted by the pod tool

--- Example/Pods/BuildHeaders/SESUsefulViews/SESCircularImageView.h
+++
@@ -1,1 +0,0 @@
-../../../../Pod/Classes/SESCircularImageView.h
--- Example/Pods/Pods-SESUsefulViews-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SESUsefulViews : NSObject
-@end
-@implementation PodsDummy_Pods_SESUsefulViews
-@end
